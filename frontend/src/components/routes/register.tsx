0df64e5d8b64a05687864601cae7c114c5f01e8c
BDeuDev <bdeusich.dev@gmail.com>
2024-04-11 16:18:17 -0300

    Validacion de formularios

--- frontend/src/components/routes/register.tsx
+++ frontend/src/components/routes/register.tsx
@@ -24,49 +24,67 @@ const Register = () => {
         'password',
     ];
     const [formData, setFormData] = useState<{ [key: string]: string }>({});
+    const [errors, setErrors] = useState<{ [key: string]: string }>({});
 
-    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setFormData({
-            ...formData,
-            [event.target.name]: event.target.value
-        });
-    };
+const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const newErrors: { [key: string]: string } = {};
+    if (!formData.username) {
+        newErrors.username = 'El nombre de usuario es requerido';
+    }
+    if (!formData.password) {
+        newErrors.password = 'La contraseña es requerida';
+    }
+    setErrors(newErrors);
 
-    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
+    if(Object.keys(newErrors).length === 0 ){
         try {
             const response = await axios.post('http://localhost:3000/user/register', formData);
             console.log(response.data);
         } catch (error) {
             console.error('Error al enviar los datos al servidor:', error);
         }
-    };
+    }
+};
 
-    return (
-        <div className="w-full h-full bg-[#F0F0F0] absolute font-raleway flex justify-center items-center">
-            <form onSubmit={handleSubmit} className="bg-white p-4 rounded-xl  w-full max-w-sm mx-auto">
-                <div className="flex flex-col items-center justify-center mb-5">
+const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setFormData({
+        ...formData,
+        [event.target.name]: event.target.value
+    });
+
+    // Limpiar el mensaje de error cuando se modifica el input
+    setErrors({
+        ...errors,
+        [event.target.name]: '',
+    });
+};
+
+return (
+    <div className="w-full h-full bg-[#F0F0F0] absolute font-raleway flex justify-center items-center">
+        <form onSubmit={handleSubmit} className="bg-white p-4 rounded-xl w-full max-w-sm mx-auto">
+        <div className="flex flex-col items-center justify-center mb-5">
                     <h1 className="text-black text-3xl font-bold">Create account</h1>
                 </div>
-                {labels.map((text, index) => (
-                    <div key={index} className="mx-2 my-3">
-                        <label htmlFor={`${names[index]}`} className="text-sm font-bold">{text}</label>
-                        <input
-                            type={index === 4 ? 'password' : 'text'}
-                            className="p-1 text-base rounded-lg shadow-sm outline-1 w-full border"
-                            placeholder={placeholders[index]}
-                            name={`${names[index]}`}
-                            onChange={handleInputChange}
-                            required
-                        />
-                    </div>
-                ))}
-                <div className="mx-2 mt-5">
+            {labels.map((text, index) => (
+                <div key={index} className="mx-2 my-3">
+                    <label htmlFor={`${names[index]}`} className="text-sm font-bold">{text}</label>
+                    <input
+                        type={index === 4 ? 'password' : 'text'}
+                        className="p-1 text-base rounded-lg shadow-sm outline-1 w-full border"
+                        placeholder={placeholders[index]}
+                        name={`${names[index]}`}
+                        onChange={handleInputChange}
+                    />
+                    {errors[names[index]] && <span style={{ color: 'red' }}>{errors[names[index]]}</span>}
+                </div>
+            ))}
+            <div className="mx-2 mt-5">
                     <input type="submit" value="Continue" className="cursor-pointer border text-lg font-bold p-3 rounded-xl w-full transition-all duration-700 ease-in-out transform text-white bg-[#143D59] bg-opacity-90 hover:bg-opacity-100 hover:outline-1 hover:outline shadow-md" />
                 </div>
-            </form>
-        </div>
-    );
+        </form>
+    </div>
+);
 }
 
 export default Register;
