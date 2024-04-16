975f6405096a4ae6b0cfa82d1c28b0f60c9e4290
BDeuDev <bdeusich.dev@gmail.com>
2024-04-15 22:13:27 -0300

    Validacion de datos de entrada del user funcionando

--- backend/dist/auth/schemas/registerData.js
+++ backend/dist/auth/schemas/registerData.js
@@ -4,11 +4,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const joi_1 = __importDefault(require("joi"));
-const UserSchema = joi_1.default.object({
+const RegisterData = joi_1.default.object({
     username: joi_1.default.string().alphanum().min(3).max(30).required(),
-    name: joi_1.default.string().pattern(new RegExp('^[a-zA-ZÀ-ÖØ-öø-ÿ]{3,30}$')).required(),
-    lastname: joi_1.default.string().pattern(new RegExp('^[a-zA-ZÀ-ÖØ-öø-ÿ]{3,30}$')).required(),
-    email: joi_1.default.string().email().required(),
-    password: joi_1.default.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).required(),
+    name: joi_1.default.string().pattern(new RegExp('^[a-zA-ZÀ-ÖØ-öø-ÿ]+$')).min(3).max(30).required(),
+    lastname: joi_1.default.string().pattern(new RegExp('^[a-zA-ZÀ-ÖØ-öø-ÿ]+$')).min(3).max(30).required(),
+    email: joi_1.default.string().pattern(new RegExp('^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$')).required(),
+    password: joi_1.default.string().pattern(new RegExp('^[a-zA-Z0-9]+$')).min(8).max(30).required(),
 });
-exports.default = UserSchema;
+exports.default = RegisterData;
--- backend/src/auth/schemas/registerData.ts
+++ backend/src/auth/schemas/registerData.ts
@@ -1,12 +1,12 @@
 import Joi from 'joi';
 
-const UserSchema = Joi.object({
+const RegisterData = Joi.object({
     username: Joi.string().alphanum().min(3).max(30).required(),
-    name: Joi.string().pattern(new RegExp('^[a-zA-ZÀ-ÖØ-öø-ÿ]{3,30}$')).required(),
-    lastname: Joi.string().pattern(new RegExp('^[a-zA-ZÀ-ÖØ-öø-ÿ]{3,30}$')).required(),
-    email: Joi.string().email().required(),
-    password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).required(),
+    name: Joi.string().pattern(new RegExp('^[a-zA-ZÀ-ÖØ-öø-ÿ]+$')).min(3).max(30).required(),
+    lastname: Joi.string().pattern(new RegExp('^[a-zA-ZÀ-ÖØ-öø-ÿ]+$')).min(3).max(30).required(),
+    email: Joi.string().pattern(new RegExp('^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$')).required(),
+    password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]+$')).min(8).max(30).required(),
 
 });
 
-export default UserSchema;
+export default RegisterData;
