65fd340a4ef4a55ab5a12da27d3373541c674a37
BDeuDev <bdeusich.dev@gmail.com>
2024-04-19 17:01:30 -0300

    cambios

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -8,7 +8,16 @@ import { CORE_URL_USER } from './config/url';
 dotenv.config();
 
 const app = express();
-
+//Mañana
+//Terminar QA
+//Inicio de sesion(Guardar tokens en session storage o local storage)
+//Enviar el token eun header de respuesta haciendo un get a nuestro endpoint
+//Almacenar en redux el state del user (logged o no)
+//y los datos del user en base a el token
+//En caso de no existir token proteger la vista del carrito para que no puedan comprar
+//el buscador siempre debe estar operativo
+//1/05 empezar con el buscador)
+//Fecha limite para terminar este proyecto 20/05
 app.use(express.json());
 app.use(cors());
 app.use('/products',productsRoutes)
